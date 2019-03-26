dc9b457efe14252834ea393c902dd522a002bae9
blackCodingCat <2569678842@qq.com>
2019-03-26 21:15:37 +0800

    项目打包上线

--- config/index.js
+++ config/index.js
@@ -9,7 +9,7 @@ module.exports = {
 
     // Paths
     assetsSubDirectory: 'static',
-    assetsPublicPath: '/',
+    assetsPublicPath: 'animating-consume/dist',
     proxyTable: {},
 
     // Various Dev Server settings
